b7a9994d43a07b8899fcf21f4e02366b5638667c
Vu Tuan Hoang <hoangvutuan@gmail.com>
2018-04-12 04:54:55 -0700

    commit FlattenNestedArray.java

---  FlattenNestedArray/src/flattennestedarray/FlattenNestedArray.java
+++  FlattenNestedArray/src/flattennestedarray/FlattenNestedArray.java
@@ -12,12 +12,12 @@ import java.util.List;
  *
  * @author vutuanhoang
  */
-public class FlattenNestedArray {
+public class FlattenNestedArray  {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception  {
         // TODO code application logic here
         
         //input:
